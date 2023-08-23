6cf3df095aca0e0ff8e0415c8acbd09952bc90be
Shaybeth2 <shaybeth2@gmail.com>
2023-08-22 17:29:28 -0700

    functions1.c

--- functions1.c
+++ functions1.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
-* print_unsigned - Prints an unsigned number
-* @types: List a of arguments
-* @buffer: Buffer array to handle print
-* @flags: Calculates active flags
+* print_unsigned -prints an unsigned number
+* @types: list a of arguments
+* @buffer: buffer array to handle print
+* @flags: calculates active flags
 * @width: get width
-* @precision: Precision specification
-* @size: Size specifier
-* Return: Number of chars printed.
+* @precision: precision specification
+* @size: size specifier
+* Return: number of chars printed.
 */
 int print_unsigned(va_list types, char buffer[],
 int flags, int width, int precision, int size)
